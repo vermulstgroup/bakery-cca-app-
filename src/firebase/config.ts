cdfc3b4aaa324a6d04fb5e7827313e3ffc6f4f38
Stefan Vermulst <Stefan@vermulstgroup.com>
2025-12-10 20:50:59 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,13 +1,14 @@
-// It appears you have requested a function that requires a Firebase project but
-// you have not created one yet. Please create a Firebase project and return to this
-// step.
-//
-// https://firebase.google.com/docs/web/setup
+
 import { FirebaseOptions, initializeApp } from 'firebase/app';
 
-const firebaseConfig: FirebaseOptions = JSON.parse(
-  process.env.NEXT_PUBLIC_FIREBASE_CONFIG || '{}'
-);
+const firebaseConfig: FirebaseOptions = {
+  "projectId": "studio-7015425522-94d4d",
+  "appId": "1:545972834050:web:d6eff076c43514a559fc1b",
+  "apiKey": "AIzaSyAuCqRysKSTmzD7HBlQugDsjToF8c7vWPc",
+  "authDomain": "studio-7015425522-94d4d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "545972834050"
+};
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
