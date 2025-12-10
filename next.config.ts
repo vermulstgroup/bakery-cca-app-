854017aa48b36647bdc2dd640bdb32f79cdc12ba
Stefan Vermulst <Stefan@vermulstgroup.com>
2025-12-10 23:26:08 +0000

    https://nextjs.org/blog/CVE-2025-66478#required-action

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,27 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
